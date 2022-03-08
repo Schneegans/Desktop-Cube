7962d34d9a0a70debcf64757a13c358ab451a677
Simon Schneegans <code@simonschneegans.de>
2022-03-08 05:38:59 +0100

    :sparkles: Tweak formatting

--- src/Skybox.js
+++ src/Skybox.js
@@ -26,124 +26,120 @@ const utils          = Me.imports.src.utils;
 // parameter controlling the horizontal and vertical rotation of the camera.            //
 //////////////////////////////////////////////////////////////////////////////////////////
 
-var SkyboxEffect = GObject.registerClass(
-  {
+// clang-format off
+var SkyboxEffect = GObject.registerClass({
     Properties: {
-      'yaw': GObject.ParamSpec.double('yaw', 'yaw', 'yaw', GObject.ParamFlags.READWRITE,
-                                      -2 * Math.PI, 2 * Math.PI, 0),
-      'pitch':
-        GObject.ParamSpec.double('pitch', 'pitch', 'pitch', GObject.ParamFlags.READWRITE,
-                                 -0.5 * Math.PI, 0.5 * Math.PI, 0),
+      'yaw':   GObject.ParamSpec.double('yaw', 'yaw', 'yaw', GObject.ParamFlags.READWRITE,
+                                        -2 * Math.PI, 2 * Math.PI, 0),
+      'pitch': GObject.ParamSpec.double('pitch', 'pitch', 'pitch', GObject.ParamFlags.READWRITE,
+                                        -0.5 * Math.PI, 0.5 * Math.PI, 0),
     },
-  },
-  class SkyboxEffect extends Shell.GLSLEffect {  // --------------------------------------
-    _init(file) {
-      super._init();
+  }, class SkyboxEffect extends Shell.GLSLEffect {
+  // clang-format on
+  _init(file) {
+    super._init();
 
-      // Attempt to load the texture.
-      const textureData = GdkPixbuf.Pixbuf.new_from_file(file);
-      this._texture     = new Clutter.Image();
-      this._texture.set_data(textureData.get_pixels(), Cogl.PixelFormat.RGB_888,
-                             textureData.width, textureData.height,
-                             textureData.rowstride);
+    // Attempt to load the texture.
+    const textureData = GdkPixbuf.Pixbuf.new_from_file(file);
+    this._texture     = new Clutter.Image();
+    this._texture.set_data(textureData.get_pixels(), Cogl.PixelFormat.RGB_888,
+                           textureData.width, textureData.height, textureData.rowstride);
 
-      // Redraw if either the pitch or the yaw changes.
-      this.connect('notify::yaw', () => {this.queue_repaint()});
-      this.connect('notify::pitch', () => {this.queue_repaint()});
-    };
+    // Redraw if either the pitch or the yaw changes.
+    this.connect('notify::yaw', () => {this.queue_repaint()});
+    this.connect('notify::pitch', () => {this.queue_repaint()});
+  };
 
-    // This is called once to setup the Cogl.Pipeline.
-    vfunc_build_pipeline() {
+  // This is called once to setup the Cogl.Pipeline.
+  vfunc_build_pipeline() {
 
-      // In the vertex shader, we compute the view space position of the actor's corners.
-      this.add_glsl_snippet(Shell.SnippetHook.VERTEX, 'varying vec4 vsPos;',
-                            'vsPos = cogl_modelview_matrix * cogl_position_in;', false);
+    // In the vertex shader, we compute the view space position of the actor's corners.
+    this.add_glsl_snippet(Shell.SnippetHook.VERTEX, 'varying vec4 vsPos;',
+                          'vsPos = cogl_modelview_matrix * cogl_position_in;', false);
 
-      const fragmentDeclares = `
-        varying vec4      vsPos;
-        uniform sampler2D uTexture;
-        uniform float     uPitch;
-        uniform float     uYaw;
+    const fragmentDeclares = `
+      varying vec4      vsPos;
+      uniform sampler2D uTexture;
+      uniform float     uPitch;
+      uniform float     uYaw;
 
-        mat3 getPitch() {
-          float s = sin(uPitch);
-          float c = cos(uPitch);
-          return mat3(1.0, 0.0, 0.0, 0.0, c, s, 0.0, -s, c);
-        }
+      mat3 getPitch() {
+        float s = sin(uPitch);
+        float c = cos(uPitch);
+        return mat3(1.0, 0.0, 0.0, 0.0, c, s, 0.0, -s, c);
+      }
 
-        mat3 getYaw() {
-          float s = sin(uYaw);
-          float c = cos(uYaw);
-          return mat3(c, 0.0, -s, 0.0, 1.0, 0.0, s, 0.0, c);
-        }
-      `;
+      mat3 getYaw() {
+        float s = sin(uYaw);
+        float c = cos(uYaw);
+        return mat3(c, 0.0, -s, 0.0, 1.0, 0.0, s, 0.0, c);
+      }
+    `;
 
-      // The fragment shader uses the interpolated viewspace position to compute a view
-      // ray. This ray is rotated according to the pitch and yaw values.
-      const fragmentCode = `
+    // The fragment shader uses the interpolated viewspace position to compute a view
+    // ray. This ray is rotated according to the pitch and yaw values.
+    const fragmentCode = `
+      // Rotate the view ray.
+      vec3 view = getYaw() * getPitch() * normalize(vsPos.xyz);
 
-        // Rotate the view ray.
-        vec3 view = getYaw() * getPitch() * normalize(vsPos.xyz);
+      // Compute equirectangular projection.
+      const float pi = 3.14159265359;
+      float x = 0.5 + 0.5 * atan(view.x, -view.z) / pi;
+      float y = acos(view.y) / pi;
 
-        // Compute equirectangular projection.
-        const float pi = 3.14159265359;
-        float x = 0.5 + 0.5 * atan(view.x, -view.z) / pi;
-        float y = acos(view.y) / pi;
+      cogl_color_out = texture2D(uTexture, vec2(x, y));
+    `;
 
-        cogl_color_out = texture2D(uTexture, vec2(x, y));
-      `;
+    this.add_glsl_snippet(Shell.SnippetHook.FRAGMENT, fragmentDeclares, fragmentCode,
+                          false);
+  }
 
-      this.add_glsl_snippet(Shell.SnippetHook.FRAGMENT, fragmentDeclares, fragmentCode,
-                            false);
-    }
+  // For each draw call, we have to set some uniform values.
+  vfunc_paint_target(node, paintContext) {
+    this.get_pipeline().set_layer_texture(0, this._texture.get_texture());
 
-    // For each draw call, we have to set some uniform values.
-    vfunc_paint_target(node, paintContext) {
-      this.get_pipeline().set_layer_texture(0, this._texture.get_texture());
+    this.set_uniform_float(this.get_uniform_location('uTexture'), 1, [0]);
+    this.set_uniform_float(this.get_uniform_location('uPitch'), 1, [this.pitch]);
+    this.set_uniform_float(this.get_uniform_location('uYaw'), 1, [this.yaw]);
 
-      this.set_uniform_float(this.get_uniform_location('uTexture'), 1, [0]);
-      this.set_uniform_float(this.get_uniform_location('uPitch'), 1, [this.pitch]);
-      this.set_uniform_float(this.get_uniform_location('uYaw'), 1, [this.yaw]);
-
-      super.vfunc_paint_target(node, paintContext);
-    }
-  });
+    super.vfunc_paint_target(node, paintContext);
+  }
+});
 
 //////////////////////////////////////////////////////////////////////////////////////////
 // The Skybox is a simple actor which applies the above effect to itself. It also has   //
 // the pitch and yaw properties - these are directly forwarded to the effect.           //
 //////////////////////////////////////////////////////////////////////////////////////////
 
-var Skybox = GObject.registerClass(
-  {
+// clang-format off
+var Skybox = GObject.registerClass({
     Properties: {
-      'yaw': GObject.ParamSpec.double('yaw', 'yaw', 'yaw', GObject.ParamFlags.READWRITE,
-                                      -2 * Math.PI, 2 * Math.PI, 0),
-      'pitch':
-        GObject.ParamSpec.double('pitch', 'pitch', 'pitch', GObject.ParamFlags.READWRITE,
-                                 -0.5 * Math.PI, 0.5 * Math.PI, 0),
+      'yaw':   GObject.ParamSpec.double('yaw', 'yaw', 'yaw', GObject.ParamFlags.READWRITE,
+                                        -2 * Math.PI, 2 * Math.PI, 0),
+      'pitch': GObject.ParamSpec.double('pitch', 'pitch', 'pitch', GObject.ParamFlags.READWRITE,
+                                        -0.5 * Math.PI, 0.5 * Math.PI, 0),
     }
-  },
-  class Skybox extends Clutter.Actor {  // -----------------------------------------------
-    _init(file) {
-      super._init();
+  }, class Skybox extends Clutter.Actor {
+  // clang-format on
+  _init(file) {
+    super._init();
 
-      // Apply the effect.
-      this._effect = new SkyboxEffect(file);
-      this.add_effect(this._effect);
+    // Apply the effect.
+    this._effect = new SkyboxEffect(file);
+    this.add_effect(this._effect);
 
-      // Make sure that the overview background is transparent.
-      Main.uiGroup.add_style_class_name('desktop-cube-panorama-enabled');
+    // Make sure that the overview background is transparent.
+    Main.uiGroup.add_style_class_name('desktop-cube-panorama-enabled');
 
-      // Forward the yaw and pitch values.
-      this.bind_property('yaw', this._effect, 'yaw', GObject.BindingFlags.NONE);
-      this.bind_property('pitch', this._effect, 'pitch', GObject.BindingFlags.NONE);
-    }
+    // Forward the yaw and pitch values.
+    this.bind_property('yaw', this._effect, 'yaw', GObject.BindingFlags.NONE);
+    this.bind_property('pitch', this._effect, 'pitch', GObject.BindingFlags.NONE);
+  }
 
-    destroy() {
-      // Revert to the original overview background appearance.
-      Main.uiGroup.remove_style_class_name('desktop-cube-panorama-enabled');
+  destroy() {
+    // Revert to the original overview background appearance.
+    Main.uiGroup.remove_style_class_name('desktop-cube-panorama-enabled');
 
-      super.destroy();
-    }
-  });
+    super.destroy();
+  }
+});
